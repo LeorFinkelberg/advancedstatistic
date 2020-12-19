82ece7516918798b16f7279cdd6708bfc443a52e
Leor Finkelberg <leor.finkelberg@yandex.ru>
2020-12-19 07:33:53 +0300

    Added main file outlier_detection.py

---
+++ advancedstatistic/outlier_detection.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# 19 дек 2020 г.  7:31:11
+# author: Podvoyskiy Alex (apodvoyskiy08@gmail.com) 
+
+
+def convert(name: str) -> str:
+    '''
+    Description
+    '''
+    print(f"I'll convert a notebook for you some day, {name}")
